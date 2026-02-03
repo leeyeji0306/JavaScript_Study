3c9893ff283c3b409b4201b279600f12b9b66e2e
leeyeji0306 <s2548@e-mirim.hs.kr>
2026-02-03 19:50:07 +0900

    2.10-Homework_repair

---
+++ javascript_study/2.10-Homework/main.js
@@ -0,0 +1,23 @@
+const calculator={
+    add: function(a, b){
+        console.log(a+b)
+    },
+    sub: function(a, b){
+        console.log(a-b)
+    },
+    div: function(a, b){
+        console.log(a/b)
+    },
+    powerof: function(a,b){
+        let power=1
+        for(let i = 0; i<b; i++){
+            power*=a
+        }
+        console.log(power)
+    }
+}
+
+calculator.add(1,2)
+calculator.sub(1,2)
+calculator.div(1,2)
+calculator.powerof(3,2)
