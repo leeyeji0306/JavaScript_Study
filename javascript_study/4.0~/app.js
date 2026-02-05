ae246ef232e77135d5c614cdd6333dfd148a6e38
leeyeji0306 <s2548@e-mirim.hs.kr>
2026-02-05 19:44:46 +0900

    4.2

--- javascript_study/4.0~/app.js
+++ javascript_study/4.0~/app.js
@@ -1,12 +1,10 @@
+const loginForm=document.querySelector("#login-form")
 const loginInput = document.querySelector("#login-form input")
 
-function onLonginBtnClick(){
-    const username = loginInput.value;
-    if(username === ""){
-        alert("Please write your name");
-    } else if(username.length > 15){
-        alert("Your name is too long.")
-    }
+function onLoginSubmit(event){
+    event.preventDefault()
+    const username=loginInput.value
+    console.log(username)
 }
 
-loginButton.addEventListener("click", onLonginBtnClick)
+loginForm.addEventListener("submit", onLoginSubmit)
