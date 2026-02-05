4d01d4a8aebd3c2b9e4815342f2ec9bdcef8f1f7
leeyeji0306 <s2548@e-mirim.hs.kr>
2026-02-06 03:22:02 +0900

    5.1

--- javascript_study/5.0~/js/clock.js
+++ javascript_study/5.0~/js/clock.js
@@ -1,7 +1,8 @@
 const clock = document.querySelector("#clock")
 
-function sayHello(){
-    console.log("hello");
+function getClock(){
+    const date = new Date()
+    clock.innerText=`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
 }
-
-setInterval(sayHello, 5000)
+getClock()
+setInterval(getClock, 1000)
