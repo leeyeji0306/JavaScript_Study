60d9c3e59731c957f3f8b09c012d4e5e173258c7
leeyeji0306 <s2548@e-mirim.hs.kr>
2026-02-06 07:08:35 +0900

    7.1

--- javascript_study/7.0~/js/todo.js
+++ javascript_study/7.0~/js/todo.js
@@ -2,10 +2,19 @@ const toDoForm=document.querySelector("#todo-form")
 const toDoInput=toDoForm.querySelector("input")
 const toDoList=document.querySelector("#todo-list")
 
+function paintToDO(newToDo){
+    const toDoListItem=document.createElement("li")
+    const span=document.createElement("span")
+    toDoListItem.appendChild(span)
+    span.innerText=newToDo
+    toDoList.appendChild(toDoListItem)
+}
+
 function handleTodoSubmit(event){
     event.preventDefault()
     const newToDo=toDoInput.value
     toDoInput.value="";
+    paintToDO(newToDo)
 }
 
 toDoForm.addEventListener("submit", handleTodoSubmit)
